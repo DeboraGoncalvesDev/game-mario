5e667ebede67527f8c3bc894d8151abd747fb9c3
DeboraGoncalvesDev <162181560+DeboraGoncalvesDev@users.noreply.github.com>
2024-06-10 16:29:28 -0300

    trabalhando com javascript

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const mario = document.querySelector('.mario');
 const jump = () => {
     mario.classList.add('jump');
 
-}
+    setTimeout(()=>{
+        mario.classList.remove('jump');
+    }, 500);
+    
+   
 
+}
+const loop = setInterval(()=> {
+    const pipePosition = pipe.offsetLeft;
+    if (pipePosition <= 120){
+        pipe.style.animation= 'none';
+    }
+},10)
 
 
 
